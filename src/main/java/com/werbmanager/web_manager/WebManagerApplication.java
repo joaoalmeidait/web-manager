109133a7e1f0b8a6a088a52994725bff31084e65
joaoalmeida <joaoalmeidapedrosa@gmail.com>
2026-03-06 19:27:21 -0300

    first commit

---
+++ src/main/java/com/werbmanager/web_manager/WebManagerApplication.java
@@ -0,0 +1,13 @@
+package com.werbmanager.web_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebManagerApplication.class, args);
+	}
+
+}
